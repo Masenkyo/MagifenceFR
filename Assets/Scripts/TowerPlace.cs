d549ade2c28173d2902049a99ec95fffb6fc3171
Masenkyo <33390@ma-web.nl>
2022-09-25 21:41:58 +0200

    IDK man

--- Assets/Scripts/TowerPlace.cs
+++ Assets/Scripts/TowerPlace.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Globalization;
+using Mono.Cecil.Cil;
+
 public class TowerPlace : MonoBehaviour
 {
     public GameObject NormalTower;
     public GameObject Tower;
+    bool CanPlace = true;
+    private GameObject Variable;
     private Camera cam => Camera.main.GetComponent<Camera>();
     Vector3 mousepos => new Vector3((Mathf.Floor(cam.ScreenToWorldPoint(Input.mousePosition).x)) + 0.5f, (Mathf.Floor(cam.ScreenToWorldPoint(Input.mousePosition).y)) + 0.5f, 0);
 
@@ -13,28 +18,54 @@ public class TowerPlace : MonoBehaviour
     {
 
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Tower"))
+        {
+            
+            
+            CanPlace = false;
+            
+        }       
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Tower"))
+        {
+            
+            CanPlace = true;
+            
+        }
+    }
 
-
-    void LateUpdate()
+    void Update()
     {
         if (Tower != null)
             Tower.transform.position = mousepos;
         if (Input.GetMouseButtonDown(0))
         {
-            Tower = null;
-            if (GameObject.FindObjectsOfType<GameObject>().Any(c => c.tag == "Tower" && c == Tower))
+            if (CanPlace == true)
             {
-
+                Debug.Log("I Believe");
+                GetComponent<Collider2D>().isTrigger = false;
+                Tower = null;
             }
-
+            if (CanPlace == false)
+            {
+                Debug.Log("hi");
+                
+            }
         }
     }
 
     public void TowerPlacee()
     {
+        
         Tower
             = Instantiate(NormalTower, mousepos, transform.rotation);
 
     }
-
+    
 }
